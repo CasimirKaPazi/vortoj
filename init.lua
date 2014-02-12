7e5387747ebb9639e4f45225ead479482b144743
Casimir <jul.lutz@gmx.net>
2014-02-12 01:36:52 +0100

    use original description when translation is empty

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ if io.open(minetest.get_modpath("vortoj").."/".. language ..".lua") then
 	dofile(minetest.get_modpath("vortoj").."/".. language ..".lua")
 
 	for _,item in ipairs(vortoj) do
-		if minetest.registered_items[item[1]] then
+		if minetest.registered_items[item[1]] and item[2] ~= "" then
 			minetest.override_item(item[1], {description = item[2]})
 		end
 	end
