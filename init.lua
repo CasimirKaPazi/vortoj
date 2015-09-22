7251a0607efd90fc0cc5ee84ffd792583cc6f6bd
Casimir <jul.lutz@gmx.net>
2015-09-22 20:43:46 +0200

    Also recognize lowercase settings

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local language = minetest.setting_get("language")
+local language = string.upper(minetest.setting_get("language"))
 
 if io.open(minetest.get_modpath("vortoj").."/".. language ..".lua") then
 	dofile(minetest.get_modpath("vortoj").."/".. language ..".lua")
